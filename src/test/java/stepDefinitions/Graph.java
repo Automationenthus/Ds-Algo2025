dc545472d771b0e9d0b380e6ca1b0e56c2f799de
sruthi-gutti <shrugutti@gmail.com>
2025-04-14 18:03:17 -0700

    Linked list, queue, Gragh features and queue step definitions

--- src/test/java/stepDefinitions/Graph.java
+++ src/test/java/stepDefinitions/Graph.java
@@ -1,5 +1,198 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Graph {
+	
+	@Given("The user is in the Home page after logged in")
+	public void the_user_is_in_the_home_page_after_logged_in() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks the {string} button of {string}")
+	public void the_user_clicks_the_button_of(String string, String string2) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should  land on Graph page of dsportalapp")
+	public void the_user_should_land_on_graph_page_of_dsportalapp() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user select Graph item from the drop down menu")
+	public void the_user_select_graph_item_from_the_drop_down_menu() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on {string} page")
+	public void the_user_is_on_page(String string) {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user lands on Graph page")
+	public void the_user_lands_on_graph_page() {
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The  user should be able to view username and signout button on top of the page")
+	public void the_user_should_be_able_to_view_username_and_signout_button_on_top_of_the_page() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be able to view NumpyNinja and DataStructures dropdown and able to perform actions on that")
+	public void the_user_should_be_able_to_view_numpy_ninja_and_data_structures_dropdown_and_able_to_perform_actions_on_that() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks on {string} link")
+	public void the_user_clicks_on_link(String string) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should land on navigate to Graph page of Graph")
+	public void the_user_should_land_on_navigate_to_graph_page_of_graph() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("user is on {string} page Of Graph")
+	public void user_is_on_page_of_graph(String string) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user clicks on Practice Questions link")
+	public void user_clicks_on_practice_questions_link() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user should land on practice page")
+	public void user_should_land_on_practice_page() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should land on  to {string} page of Graph and should see Run button")
+	public void the_user_should_land_on_to_page_of_graph_and_should_see_run_button(String string) {
+	 
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on {string} page of {string} page")
+	public void the_user_is_on_page_of_page(String string, String string2) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks on run button by writing incorrect pyhton code")
+	public void the_user_clicks_on_run_button_by_writing_incorrect_pyhton_code() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should see the error message in alert window along with line number")
+	public void the_user_should_see_the_error_message_in_alert_window_along_with_line_number() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on\"try Editor\" page of {string} page")
+	public void the_user_is_on_try_editor_page_of_page(String string) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks on run button without  writing code in editor")
+	public void the_user_clicks_on_run_button_without_writing_code_in_editor() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should see the error message in alert window")
+	public void the_user_should_see_the_error_message_in_alert_window() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is on {string} page with alert window open of {string} page")
+	public void the_user_is_on_page_with_alert_window_open_of_page(String string, String string2) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user did not clicks on {string} button on alert window")
+	public void the_user_did_not_clicks_on_button_on_alert_window(String string) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should not able to perform any other operation on page")
+	public void the_user_should_not_able_to_perform_any_other_operation_on_page() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks on {string} button on alert window")
+	public void the_user_clicks_on_button_on_alert_window(String string) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should be able to edit code on editor and perform other operations as well")
+	public void the_user_should_be_able_to_edit_code_on_editor_and_perform_other_operations_as_well() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Given("The user is in the tryEditor page")
+	public void the_user_is_in_the_try_editor_page() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user write the valid code in Editor and click the Run Button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
+	    
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should able to see output in the console")
+	public void the_user_should_able_to_see_output_in_the_console() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should land on navigate to Graph Representations page of Graph")
+	public void the_user_should_land_on_navigate_to_graph_representations_page_of_graph() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The user should land on  to {string} page of Graph Representations and should see Run button")
+	public void the_user_should_land_on_to_page_of_graph_representations_and_should_see_run_button(String string) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user clicks on Signout button")
+	public void the_user_clicks_on_signout_button() {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("Then user should be navigate back to {string} page and {string} message should be displayed on home page")
+	public void then_user_should_be_navigate_back_to_page_and_message_should_be_displayed_on_home_page(String string, String string2) {
+	   
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+
 
 }
